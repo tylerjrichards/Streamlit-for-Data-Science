5ebc952f8cc9951e78f8a6d66db02fc165ca6483
tylerjrichards <tylerjrichards@gmail.com>
2021-01-10 20:34:05 -0800

    added viz section

--- trees_app/trees.py
+++ trees_app/trees.py
@@ -1,5 +1,16 @@
 import streamlit as st
 import pandas as pd
+from bokeh.plotting import figure
 
+st.title('SF Trees')
+st.write('This app analyses trees in San Francisco using'
+         ' a dataset kindly provided by SF DPW')
+st.subheader('Plotly Chart')
 trees_df = pd.read_csv('trees.csv')
-st.write(trees_df.head())
+
+scatterplot = figure(title = 'Bokeh Scatterplot')
+scatterplot.scatter(trees_df['dbh'], trees_df['site_order'])
+scatterplot.yaxis.axis_label = "site_order"
+scatterplot.xaxis.axis_label = "dbh"
+st.bokeh_chart(scatterplot)
+
