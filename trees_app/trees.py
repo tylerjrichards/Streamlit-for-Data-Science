699fe83d84c438f1137222d62ee520e9f8ac90b7
tylerjrichards <tylerjrichards@gmail.com>
2021-01-17 10:23:27 -0800

    viz chapter edits

--- trees_app/trees.py
+++ trees_app/trees.py
@@ -1,6 +1,7 @@
+
 import streamlit as st
 import pandas as pd
-from bokeh.plotting import figure
+import plotly.express as px
 
 st.title('SF Trees')
 st.write('This app analyses trees in San Francisco using'
@@ -8,9 +9,5 @@ st.write('This app analyses trees in San Francisco using'
 st.subheader('Plotly Chart')
 trees_df = pd.read_csv('trees.csv')
 
-scatterplot = figure(title = 'Bokeh Scatterplot')
-scatterplot.scatter(trees_df['dbh'], trees_df['site_order'])
-scatterplot.yaxis.axis_label = "site_order"
-scatterplot.xaxis.axis_label = "dbh"
-st.bokeh_chart(scatterplot)
-
+fig = px.histogram(trees_df['dbh'])
+st.plotly_chart(fig)
