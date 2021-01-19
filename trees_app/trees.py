f19d1b7dd16d7bc9f3e3b5bc8eab402fa3327937
tylerjrichards <tylerjrichards@gmail.com>
2021-01-18 16:14:05 -0800

    viz finished

--- trees_app/trees.py
+++ trees_app/trees.py
@@ -1,13 +1,29 @@
-
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+import pydeck as pdk 
 
 st.title('SF Trees')
 st.write('This app analyses trees in San Francisco using'
          ' a dataset kindly provided by SF DPW')
-st.subheader('Plotly Chart')
 trees_df = pd.read_csv('trees.csv')
+trees_df.dropna(how='any', inplace=True)
 
-fig = px.histogram(trees_df['dbh'])
-st.plotly_chart(fig)
+sf_initial_view = pdk.ViewState(
+	latitude=37.77,
+	longitude=-122.4,
+	zoom=11,
+	pitch=30
+	)
+
+hx_layer = pdk.Layer(
+	'HexagonLayer',
+	data = trees_df,
+	get_position = ['longitude', 'latitude'],
+	radius=100,
+	extruded=True)
+
+st.pydeck_chart(pdk.Deck(
+	map_style='mapbox://styles/mapbox/light-v9',
+	initial_view_state=sf_initial_view,
+	layers = [hx_layer]
+	))
