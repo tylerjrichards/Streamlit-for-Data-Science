b466b4cd1d5867483585aaa1f28c4176328229eb
Tyler Richards <tylerjrichards@fb.com>
2021-05-11 22:13:39 -0700

    added secrets

--- penguin_ml/penguins_streamlit.py
+++ penguin_ml/penguins_streamlit.py
@@ -12,6 +12,10 @@ st.write("This app uses 6 inputs to predict the species of penguin using "
          "a model built on the Palmer's Penguin's dataset. Use the form below"
          " to get started!")
 
+password_guess = st.text_input('What is the Password?')
+if password_guess != st.secrets["password"]:
+  st.stop()
+
 penguin_file = st.file_uploader('Upload your own penguin data')
 
 if penguin_file is None:
