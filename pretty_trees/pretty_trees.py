1cf724d55ae729b94b996805c49ec587b8fff799
Tyler Richards <tylerjrichards@fb.com>
2021-06-20 20:21:30 -0400

    last check fixes

--- pretty_trees/pretty_trees.py
+++ pretty_trees/pretty_trees.py
@@ -4,20 +4,37 @@ import pandas as pd
 
  
 
+trees_df = pd.read_csv('trees.csv') 
+
+owners = st.sidebar.multiselect( 
+
+'Tree Owner Filter', trees_df['caretaker'].unique()) 
+
+ 
+
 st.title('SF Trees') 
 
 st.write('This app analyses trees in San Francisco using' 
 
-         ' a dataset kindly provided by SF DPW') 
+         ' a dataset kindly provided by SF DPW. The ' 
 
-trees_df = pd.read_csv('trees.csv') 
+         'histogram below is filtered by tree owner.') 
 
-df_dbh_grouped = pd.DataFrame(trees_df.groupby(['dbh']).count()['tree_id']) 
+st.write('The current analysis is of trees owned by {}'.format(owners)) 
 
-df_dbh_grouped.columns = ['tree_count'] 
+ 
 
-st.line_chart(df_dbh_grouped) 
+if owners: 
 
-st.bar_chart(df_dbh_grouped) 
+    trees_df = trees_df[trees_df['caretaker'].isin(owners)]  
+    df_dbh_grouped = pd.DataFrame(trees_df.groupby(['dbh']).count()['tree_id']) 
+    df_dbh_grouped.columns = ['tree_count'] 
+    st.line_chart(df_dbh_grouped) 
 
-st.area_chart(df_dbh_grouped) 
+ 
+
+trees_df = trees_df.dropna(subset=['longitude', 'latitude']) 
+
+trees_df = trees_df.sample(n = 1000, replace=True) 
+
+st.map(trees_df) 
