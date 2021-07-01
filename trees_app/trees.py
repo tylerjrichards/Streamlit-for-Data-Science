68936b03bd17f5e1c9eb3da8bfb4fa8b299b1c08
Tyler Richards <tylerjrichards@fb.com>
2021-06-30 21:30:54 -0400

    mistakes

--- trees_app/trees.py
+++ trees_app/trees.py
@@ -7,8 +7,8 @@ st.write('This app analyses trees in San Francisco using'
          ' a dataset kindly provided by SF DPW')
 
 trees_df = pd.read_csv('trees.csv')
-#trees_df.dropna(how='any', inplace=True)
-'''
+trees_df.dropna(how='any', inplace=True)
+
 sf_initial_view = pdk.ViewState(
     latitude=37.77,
     longitude=-122.4,
@@ -26,4 +26,3 @@ st.pydeck_chart(pdk.Deck(
     initial_view_state=sf_initial_view,
     layers = [sp_layer]
     ))
-    '''
