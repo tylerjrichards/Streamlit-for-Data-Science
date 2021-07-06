05673c10cb87ba8ba644cc6a3d52149597dd4fb6
Tyler Richards <tylerjrichards@fb.com>
2021-07-06 19:23:05 -0400

    added chapter 11 material

--- clt_app/clt_demo.py
+++ clt_app/clt_demo.py
@@ -1,19 +1,30 @@
-import streamlit as st   
-import numpy as np   
+import streamlit as st 
+
+import numpy as np 
+
 import matplotlib.pyplot as plt 
 
  
 
-perc_heads = st.number_input(label='Chance of Coins Landing on Heads', min_value=0.0,   max_value=1.0, value=.5)  
-graph_title = st.text_input(label='Graph Title') 
-binom_dist = np.random.binomial(1, perc_heads, 1000)  
-list_of_means = []  
-for i in range(0, 1000):  
-    list_of_means.append(np.random.choice(binom_dist, 100, replace=True).mean())  
+st.title('Illustrating the Central Limit Theorem with Streamlit') 
+st.subheader('An App by Tyler Richards') 
+st.write(('This app simulates a thousand coin flips using the chance of heads input below,'  
+'and then samples with replacement from that population and plots the histogram of the' 
+' means of the samples, in order to illustrate the Central Limit Theorem!')) 
 
-  
+ 
 
-fig, ax = plt.subplots()  
-plt.hist(list_of_means, range=[0,1]) 
-plt.title(graph_title) 
+perc_heads = st.number_input(label='Chance of Coins Landing on Heads', min_value=0.0, max_value=1.0, value=.5) 
+binom_dist = np.random.binomial(1, perc_heads, 1000) 
+
+ 
+
+list_of_means = [] 
+for i in range(0, 1000): 
+    list_of_means.append(np.random.choice(binom_dist, 100, replace=True).mean()) 
+
+ 
+
+fig, ax = plt.subplots() 
+ax = plt.hist(list_of_means) 
 st.pyplot(fig) 
