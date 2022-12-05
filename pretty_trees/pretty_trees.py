78b31649d04180346c01a180b04fbf68057bc0aa
tyler.richards <tylerjrichards@gmail.com>
2022-12-05 08:51:56 -0800

    map edits

--- pretty_trees/pretty_trees.py
+++ pretty_trees/pretty_trees.py
@@ -3,23 +3,25 @@ import datetime as dt
 import pandas as pd
 import streamlit as st
 
-st.set_page_config(layout="wide")
 st.title("SF Trees")
 st.write(
     """
     This app analyses trees in San Francisco using
-    a dataset kindly provided by SF DPW.
+    a dataset kindly provided by SF DPW. The dataset
+    is filtered by the owner of the tree as selected
+    in the sidebar!
     """
 )
 trees_df = pd.read_csv("trees.csv")
+owners = st.sidebar.multiselect("Tree Owner Filter", trees_df["caretaker"].unique())
+if owners:
+    trees_df = trees_df[trees_df["caretaker"].isin(owners)]
 df_dbh_grouped = pd.DataFrame(trees_df.groupby(["dbh"]).count()["tree_id"])
 df_dbh_grouped.columns = ["tree_count"]
-col1, col2, col3 = st.columns(3, gap="large")
-with col1:
-    st.line_chart(df_dbh_grouped, use_container_width=False)
-with col2:
-    st.bar_chart(df_dbh_grouped)
-with col3:
-    st.area_chart(df_dbh_grouped)
+st.line_chart(df_dbh_grouped)
+
+trees_df = trees_df.dropna(subset=["longitude", "latitude"])
+trees_df = trees_df.sample(n=1000, replace=True)
+st.map(trees_df)
 
 st.stop()
