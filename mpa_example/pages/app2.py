a78ad592dd6c88a15ebcbeca8f892bb117af36b4
tyler.richards <tylerjrichards@gmail.com>
2022-12-08 21:55:10 -0800

    added mpa example

---
+++ mpa_example/pages/app2.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("MPA Example: App 2")
+st.write("Here is our second page in our multipage app!")
--- pretty_trees/pretty_trees.py
+++ pretty_trees/pretty_trees.py
@@ -1,7 +1,10 @@
 import datetime as dt
 
 import pandas as pd
+import plotly.express as px
 import streamlit as st
+
+st.set_page_config(layout="wide")
 
 st.title("SF Trees")
 st.write(
@@ -13,15 +16,43 @@ st.write(
     """
 )
 trees_df = pd.read_csv("trees.csv")
-owners = st.sidebar.multiselect("Tree Owner Filter", trees_df["caretaker"].unique())
+today = pd.to_datetime("today")
+trees_df["date"] = pd.to_datetime(trees_df["date"])
+trees_df["age"] = (today - trees_df["date"]).dt.days
+unique_caretakers = trees_df["caretaker"].unique()
+owners = st.sidebar.multiselect("Tree Owner Filter", unique_caretakers)
+graph_color = st.sidebar.color_picker("Graph Colors")
 if owners:
     trees_df = trees_df[trees_df["caretaker"].isin(owners)]
-df_dbh_grouped = pd.DataFrame(trees_df.groupby(["dbh"]).count()["tree_id"])
-df_dbh_grouped.columns = ["tree_count"]
-st.line_chart(df_dbh_grouped)
 
+
+col1, col2 = st.columns(2)
+with col1:
+    fig = px.histogram(
+        trees_df,
+        x=trees_df["dbh"],
+        title="Tree Width",
+        color_discrete_sequence=[graph_color],
+    )
+    fig.update_xaxes(title_text="Width")
+    st.plotly_chart(fig, use_container_width=True)
+
+with col2:
+    fig = px.histogram(
+        trees_df,
+        x=trees_df["age"],
+        title="Tree Age",
+        color_discrete_sequence=[graph_color],
+    )
+    st.plotly_chart(fig, use_container_width=True)
+
+st.write("Trees by Location")
 trees_df = trees_df.dropna(subset=["longitude", "latitude"])
 trees_df = trees_df.sample(n=1000, replace=True)
 st.map(trees_df)
 
 st.stop()
+
+
+df_dbh_grouped = pd.DataFrame(trees_df.groupby(["dbh"]).count()["tree_id"])
+df_dbh_grouped.columns = ["tree_count"]
