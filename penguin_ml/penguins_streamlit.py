8301fb9eecb93bdb6a7af1a3705525f7084dbe4e
tyler.richards <tylerjrichards@gmail.com>
2023-04-22 17:58:12 -0700

    added password back

--- penguin_ml/penguins_streamlit.py
+++ penguin_ml/penguins_streamlit.py
@@ -17,8 +17,9 @@ st.write(
      Use the form below to get started!"""
 )
 
-
-
+password_guess = st.text_input("What is the Password?")
+if password_guess != "streamlit_is_great":
+    st.stop()
 
 
 penguin_file = st.file_uploader("Upload your own penguin data")
