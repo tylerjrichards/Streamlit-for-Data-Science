001f615087f1b3d620e22ad9284fb9cbaf2c10d9
tyler.richards <tylerjrichards@gmail.com>
2023-08-27 11:52:35 -0600

    added openai

--- huggingface_demo/streamlit_app.py
+++ huggingface_demo/streamlit_app.py
@@ -1,3 +1,4 @@
+import openai
 import streamlit as st
 from transformers import pipeline
 
@@ -5,7 +6,7 @@ st.title("Hugging Face Demo")
 text = st.text_input("Enter text to analyze")
 
 
-@st.experimental_singleton()
+@st.cache_resource()
 def get_model():
     return pipeline("sentiment-analysis")
 
@@ -15,3 +16,22 @@ if text:
     result = model(text)
     st.write("Sentiment:", result[0]["label"])
     st.write("Confidence:", result[0]["score"])
+
+st.title("OpenAI Version")
+
+analyze_button = st.button('Analyze Text')
+openai.api_key = st.secrets["OPENAI_API_KEY"]
+
+if analyze_button:
+    messages = [
+        {"role": "system", "content": """You are a helpful sentiment analysis assistant.
+            You always respond with the sentiment of the text you are given and the confidence of your sentiment
+            analysis with a number between 0 and 1"""},
+        {"role": "user", "content": f"Sentiment analysis of the following text: {text}"}
+    ]
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+    )
+    sentiment = response.choices[0].message['content'].strip()
+    st.write(sentiment)
