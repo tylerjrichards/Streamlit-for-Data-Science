191cadc153cf2724b205d5da2f2576e98519eb9c
tyler.richards <tylerjrichards@gmail.com>
2023-08-31 21:20:45 -0600

    system messages

--- huggingface_demo/streamlit_app.py
+++ huggingface_demo/streamlit_app.py
@@ -19,19 +19,30 @@ if text:
 
 st.title("OpenAI Version")
 
-analyze_button = st.button('Analyze Text')
+
 openai.api_key = st.secrets["OPENAI_API_KEY"]
+
+system_message_default = """You are a helpful sentiment analysis assistant. You always respond with the sentiment of the text you are given and the confidence of your sentiment analysis with a number between 0 and 1"""
+
+system_message = st.text_area(
+    "Enter a System Message to instruct OpenAI", system_message_default
+)
+analyze_button = st.button("Analyze Text")
 
 if analyze_button:
     messages = [
-        {"role": "system", "content": """You are a helpful sentiment analysis assistant.
-            You always respond with the sentiment of the text you are given and the confidence of your sentiment
-            analysis with a number between 0 and 1"""},
-        {"role": "user", "content": f"Sentiment analysis of the following text: {text}"}
+        {
+            "role": "system",
+            "content": f"{system_message}",
+        },
+        {
+            "role": "user",
+            "content": f"Sentiment analysis of the following text: {text}",
+        },
     ]
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages,
     )
-    sentiment = response.choices[0].message['content'].strip()
+    sentiment = response.choices[0].message["content"].strip()
     st.write(sentiment)
