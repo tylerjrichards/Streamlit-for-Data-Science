5ef0549a7b3eecfb9a2ae9e6f47aa0360ed95e21
tyler.richards <tylerjrichards@gmail.com>
2023-09-08 11:08:58 -0600

    version lock

--- clt_app/example.py
+++ clt_app/example.py
@@ -1,18 +1,23 @@
-import streamlit as st   
-import numpy as np   
-import matplotlib.pyplot as plt  
+import matplotlib.pyplot as plt
+import numpy as np
+import streamlit as st
 
-with st.form('first form'):
-	perc_heads = st.number_input(label='Chance of Coins Landing on Heads', min_value=0.0, 	max_value=1.0, value=.5)  
-	graph_title = st.text_input(label='Graph Title') 
-	my_submit_button = st.form_submit_button()
+with st.form("first form"):
+    perc_heads = st.number_input(
+        label="Chance of Coins Landing on Heads",
+        min_value=0.0,
+        max_value=1.0,
+        value=0.5,
+    )
+    graph_title = st.text_input(label="Graph Title")
+    my_submit_button = st.form_submit_button()
 
-binom_dist = np.random.binomial(1, perc_heads, 1000)  
-list_of_means = []  
+binom_dist = np.random.binomial(1, perc_heads, 1000)
+list_of_means = []
 for i in range(0, 1000):
-	list_of_means.append(np.random.choice(binom_dist, 100, replace=True).mean())  
+    list_of_means.append(np.random.choice(binom_dist, 100, replace=True).mean())
 
-fig, ax = plt.subplots()  
-plt.hist(list_of_means, range=[0,1]) 
-plt.title(graph_title) 
-st.pyplot(fig) 
+fig, ax = plt.subplots()
+plt.hist(list_of_means, range=[0, 1])
+plt.title(graph_title)
+st.pyplot(fig)
