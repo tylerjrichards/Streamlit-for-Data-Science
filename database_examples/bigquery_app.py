23fd502c1c0f93b22e002f89434a5fe0358c10f7
tyler.richards <tylerjrichards@gmail.com>
2023-09-20 22:04:47 -0400

    api vs service account

--- database_examples/bigquery_app.py
+++ database_examples/bigquery_app.py
@@ -7,11 +7,13 @@ from queries import get_streamlit_pypi_data
 @st.cache_resource
 def get_bigquery_client():
     credentials = service_account.Credentials.from_service_account_info(
-        st.secrets["bigquery_api"]
+        st.secrets["bigquery_service_account"]
     )
     return bigquery.Client(credentials=credentials)
 
+
 client = get_bigquery_client()
+
 
 @st.cache_data
 def get_dataframe_from_sql(query):
@@ -20,7 +22,9 @@ def get_dataframe_from_sql(query):
 
 
 st.title("BigQuery App")
-days_lookback = st.slider('How many days of data do you want to see?', min_value=1, max_value=30, value=5)
+days_lookback = st.slider(
+    "How many days of data do you want to see?", min_value=1, max_value=30, value=5
+)
 pypi_query = get_streamlit_pypi_data(days_lookback)
 
 downloads_df = get_dataframe_from_sql(pypi_query)
